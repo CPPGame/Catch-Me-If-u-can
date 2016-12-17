244156271c0b35dd8b363dbc5cc57da56ac25f55
Kevin V <kvlrio@users.noreply.github.com>
2016-12-17 21:12:57 +0100

    Creation des obstacles randomizées, bordures crées,

--- main.cpp
+++ main.cpp
@@ -1,293 +1,359 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <utility>
+#include <stdlib.h>   
+#include <time.h>    
+#include <random>
+
+
 using namespace std;
 
+const string KReset("0");
+const string KNoir("30");
+const string KRouge("31");
+const string KVert("32");
+const string KJaune("33");
+const string KBleu("34");
+const string KMagenta("35");
+const string KCyan("36");
+
+const char KEmpty = '.';
+const char KObstacle = 'O';
+const char KBorderLine = '_';
+const char KBorderColumn = '|';
+typedef vector <char> CVLine;
+typedef vector <CVLine> CMatrix;
+
 namespace {
-const string KReset   ("0");
-const string KNoir    ("30");
-const string KRouge   ("31");
-const string KVert    ("32");
-const string KJaune   ("33");
-const string KBleu    ("34");
-const string KMAgenta ("35");
-const string KCyan    ("36");
-const char KEmpty = '-';
+	typedef struct {
+		unsigned m_X;
+		unsigned m_Y;
+		unsigned m_sizeX;
+		unsigned m_sizeY;
+		char m_token;
+		string m_color;
+	} SPlayer;
 
-typedef vector <char> CVLine; // un type représentant une ligne de la grille
-typedef vector <CVLine> CMatrix; // un type représentant la grille
+ 	typedef struct {
+		unsigned m_X;
+		unsigned m_Y;
+		char m_token;
+		string m_color;
+	} SObstacle;
 
-typedef struct {
-    unsigned posX;
-    unsigned posY;
-    unsigned sizeX;
-    unsigned sizeY;
-    char token;
-    string color;
-} Player;
+	typedef struct {
+		unsigned m_X;
+		unsigned m_Y;
+		unsigned m_sizeX;
+		unsigned m_sizeY;
+		char m_token;
+		string m_color;
+	} SBonus;
 
-typedef struct {
-    unsigned posX;
-    unsigned posY;
-    char token;
-    string color;
-} Obstacle;
+	//AUTRE 
+	void Couleur(const string & coul) {
+		cout << "\033[" << coul << "m";
 
-void Couleur (const string & coul);
-void ClearScreen ();
-void ShowMatrix (const CMatrix & Mat,Player & FirstPlayer, Player & SecondPlayer);
-void InitMat (CMatrix & Mat, unsigned NbLine, unsigned NbColumn, Player & FirstPlayer, Player & SecondPlayer);
-void MoveToken (CMatrix & Mat, char Move, Player & player);
-void GetBonus(CMatrix & Mat, Player & player);
-void PutBonus(CMatrix & Mat, const char & token, const unsigned & posX, const unsigned & posY);
-bool CheckWin(Player & FirstPlayer, Player & SecondPlayer);
-int ppal();
+	}
 
-void Couleur (const string & coul)
-{
-    cout << "\033[" << coul <<"m";
+	void ClearScreen() {
+		cout << "\033[H\033[2J";
+	}
 
-}
+	int IntRandom(int min, int max) { // Taken from Stackoverflow
+		random_device randm;
+		mt19937 rng(randm());
+		uniform_int_distribution<int> uni(min, max);
 
-void ClearScreen ()
-{
-    cout << "\033[H\033[2J";
-}
-
-void ShowMatrix (const CMatrix & Mat,Player & FirstPlayer, Player & SecondPlayer)
-{
-    ClearScreen();
-    Couleur (KReset);
-    for (CVLine line : Mat)
-    {
-        for (char C : line)
-        {
-            if (C == FirstPlayer.token) Couleur (FirstPlayer.color);
-            if (C == SecondPlayer.token) Couleur (SecondPlayer.color);
-            cout << C;
-            if ((C == FirstPlayer.token)||(C == SecondPlayer.token)) Couleur (KReset);
-        }
-        cout << endl;
-    }
-}
-
-void InitMat (CMatrix & Mat, unsigned NbLine, unsigned NbColumn, Player & FirstPlayer, Player & SecondPlayer)
-{
-    Mat.resize(NbLine);
-    for (unsigned i (0); i<NbLine; ++i)
-    {
-        for (unsigned j (0); j<NbColumn; ++j)
-        {
-            Mat[i].push_back(KEmpty);
-        }
-    }
-    for (unsigned i (FirstPlayer.posY); i<FirstPlayer.posY + FirstPlayer.sizeY; ++i)
-        for (unsigned j (FirstPlayer.posX); j<FirstPlayer.posX + FirstPlayer.sizeX; ++j)
-            Mat[i][j] = FirstPlayer.token;
-    for (unsigned i (SecondPlayer.posY); i<SecondPlayer.posY + SecondPlayer.sizeY; ++i)
-        for (unsigned j (SecondPlayer.posX); j<SecondPlayer.posX + SecondPlayer.sizeX; ++j)
-            Mat[i][j] = SecondPlayer.token;
-}
-
-void MoveToken (CMatrix & Mat, char Move, Player & player)
-{
-    switch (Move)
-    {
-    case 'z':
-        if (player.posY > 0)
-        {
-            player.posY = player.posY - 1;
-            GetBonus(Mat, player);
-            for (unsigned i (player.posX); i < player.posX + player.sizeX; ++i)
-                Mat[player.posY + player.sizeY][i] = KEmpty;
-            for (unsigned i (player.posX); i < player.posX + player.sizeX; ++i)
-                Mat[player.posY][i] = player.token;
-        }
-        break;
-    case 's':
-        if (player.posY + player.sizeY < Mat.size())
-        {
-            player.posY = player.posY + 1;
-            GetBonus(Mat, player);
-            for (unsigned i (player.posX); i < player.posX + player.sizeX; ++i)
-                Mat[player.posY - 1][i] = KEmpty;
-            for (unsigned i (player.posX); i < player.posX + player.sizeX; ++i)
-                Mat[player.posY + player.sizeY - 1][i] = player.token;
-
-        }
-        break;
-    case 'q':
-        if (player.posX > 0)
-        {
-            player.posX = player.posX - 1;
-            GetBonus(Mat, player);
-            for (unsigned i (player.posY); i < player.posY + player.sizeY; ++i)
-                Mat[i][player.posX + player.sizeX] = KEmpty;
-            for (unsigned i (player.posY); i < player.posY + player.sizeY; ++i)
-                Mat[i][player.posX] = player.token;
-        }
-        break;
-    case 'd':
-        if (player.posX + player.sizeX < Mat[0].size())
-        {
-            player.posX = player.posX + 1;
-            GetBonus(Mat, player);
-            for (unsigned i (player.posY); i < player.posY + player.sizeY; ++i)
-                Mat[i][player.posX - 1] = KEmpty;
-            for (unsigned i (player.posY); i < player.posY + player.sizeY; ++i)
-                Mat[i][player.posX + player.sizeX - 1] = player.token;
-        }
-        break;
-    }
-}
-
-void PutBonus(CMatrix & Mat, const char & token, const unsigned & posX, const unsigned & posY)
-{
-    Mat[posY][posX] = token;
-}
-
-void GetBonus(CMatrix & Mat, Player & player)
-{
-    for (unsigned i (player.posY); i<player.posY + player.sizeY; ++i)
-    {
-        for (unsigned j (player.posX); j<player.posX + player.sizeX; ++j)
-        {
-            if (Mat[i][j] == 'B')
-            {
-                ++player.sizeX;
-                ++player.sizeY;
-                for (unsigned i (player.posY); i<player.posY + player.sizeY; ++i)
-                    for (unsigned j (player.posX); j<player.posX + player.sizeX; ++j)
-                        Mat[i][j] = player.token;
-            }
-        }
-    }
-}
-
-bool CheckIfWin(Player & FirstPlayer, Player & SecondPlayer)
-{
-    return !((FirstPlayer.posX > SecondPlayer.posX + SecondPlayer.sizeX - 1) ||
-             (FirstPlayer.posX + FirstPlayer.sizeX - 1 < SecondPlayer.posX) ||
-             (SecondPlayer.posY > FirstPlayer.posY + FirstPlayer.sizeY - 1)  ||
-             (SecondPlayer.posY + SecondPlayer.sizeY - 1 < FirstPlayer.posY));
-}
-
-char GetWinner(Player& FirstPlayer, Player &SecondPlayer, const unsigned & NbrTour){
-    return (NbrTour%2 == 0 ? FirstPlayer.token : SecondPlayer.token);
-}
-
-Player InitPlayer(const unsigned  largeur, const unsigned  hauteur, const unsigned  AxeX, const unsigned  AxeY, const char  Token,  const string  Color){
-
-    Player tmpPlayer;
-
-    tmpPlayer.sizeX = largeur;
-    tmpPlayer.sizeY = hauteur;
-    tmpPlayer.posX = AxeX;
-    tmpPlayer.posY = AxeY;
-    tmpPlayer.token = Token;
-    tmpPlayer.color = Color;
-
-    return tmpPlayer;
-}
-
-int Randomize(int Min, int Max){
-    return rand()%(Max-Min) +Min;
-}
-
-Obstacle CreateObstacle(const unsigned AxeX, const unsigned AxeY, const char Token, const string Color){
-
-    Obstacle tmpObstacle;
-
-    tmpObstacle.posX = AxeX;
-    tmpObstacle.posY = AxeY;
-    tmpObstacle.token = Token;
-    tmpObstacle.color = Color;
+		return uni(rng);
+	}
 
 
-    return tmpObstacle;
+	// MATRICE
 
-}
+	CMatrix InitMat(unsigned NbLine, unsigned NbColumn, SPlayer & FirstPlayer, SPlayer & SecondPlayer, bool ShowBorder) {
+		CMatrix tmpMatrix;
+		tmpMatrix.resize(NbLine);
 
-bool CanMove(Player Joueur){
-
-}
-
-void PutObstacle(CMatrix & Matrice, Obstacle & obstacle){
-    Matrice[obstacle.posX][obstacle.posY] = obstacle.token;
-}
-
-void GenerateRandomObstacle(CMatrix & Matrice,  Obstacle & obstacle,  int TailleMax){
-
-    Obstacle tmpObstacle = CreateObstacle(obstacle.posX, obstacle.posY, obstacle.token, obstacle.color);
-    for(unsigned i(0); i < TailleMax; ++i){
-        PutObstacle(Matrice, tmpObstacle);
-        tmpObstacle.posX += 1;
-        tmpObstacle.posY += 1;
-
-    }
-}
+		for (unsigned i(0); i < NbLine; ++i)
+			for (unsigned j(0); j < NbColumn; ++j)
+				tmpMatrix[i].push_back(KEmpty);
 
 
-unsigned AskTourMax(){
-    unsigned NbRnds;
-    cout << "Entrez le nombre de rounds" << endl;
-    cin >> NbRnds;
-    return NbRnds;
-}
 
-int ppal ()
-{
-    // Taille matrice
-    const unsigned KSizeX (10);
-    const unsigned KSizeY (10);
-
-    unsigned NbRnds = AskTourMax();
-    char EnteredKey;
-
-    //Joueurs & Matrice & Obstacles
-    CMatrix Map;
-
-    Player FirstPlayer = InitPlayer( 1, 1, 0, 0, 'X', KRouge);
-    Player SecondPlayer = InitPlayer(1, 1, KSizeX - 1, KSizeY - 1, 'Y', KBleu);
-
-    Obstacle FirstObstacle = CreateObstacle(5, 5, '|', KMAgenta);
-    GenerateRandomObstacle(Map, FirstObstacle, 3);
+		for (unsigned i(FirstPlayer.m_Y); i < FirstPlayer.m_Y + FirstPlayer.m_sizeY; ++i)
+			for (unsigned j(FirstPlayer.m_X); j < FirstPlayer.m_X + FirstPlayer.m_sizeX; ++j)
+				tmpMatrix[i][j] = FirstPlayer.m_token;
 
 
-    //Création de la matrice
-    InitMat(Map, KSizeX, KSizeY, FirstPlayer, SecondPlayer);
+		for (unsigned i(SecondPlayer.m_Y); i < SecondPlayer.m_Y + SecondPlayer.m_sizeY; ++i)
+			for (unsigned j(SecondPlayer.m_X); j < SecondPlayer.m_X + SecondPlayer.m_sizeX; ++j)
+				tmpMatrix[i][j] = SecondPlayer.m_token;
 
-    //Création des bonus
-    PutBonus(Map,'B',2,2);
+		if (ShowBorder) {
+			for (unsigned i(0); i < NbLine; ++i) {
 
-    ShowMatrix(Map, FirstPlayer, SecondPlayer);
+				tmpMatrix[i][0] = KBorderColumn;
+				tmpMatrix[i][NbColumn - 1] = KBorderColumn;
+			}
 
-    for (unsigned i (0); i < NbRnds*2; ++i)
-    {
+			for (unsigned i(0); i < NbColumn; ++i) {
+				tmpMatrix[0][i] = KBorderLine;
+				tmpMatrix[NbLine - 1][i] = KBorderLine;
+			}
 
-        ShowMatrix(Map, FirstPlayer, SecondPlayer);
-        Player & actualPlayer = (i%2 == 0 ? FirstPlayer : SecondPlayer);
-
-        cout << "Au tour de " << actualPlayer.token << endl;
-        cin >> EnteredKey;
+		}
 
 
-        if(CanMove(actualPlayer))   MoveToken(Map, EnteredKey, actualPlayer);
+		return tmpMatrix;
+	}
 
-        if (CheckIfWin(FirstPlayer, SecondPlayer))
-        {
-            cout << GetWinner(FirstPlayer, SecondPlayer, i) << " a gagné !" << endl;
-            return 0;
-        }
-    }
-    cout << "Egalité !" << endl;
-    return 0;
+	void ShowMatrix(const CMatrix & Mat, SPlayer & FirstPlayer, SPlayer & SecondPlayer) {
+		ClearScreen();
+		Couleur(KReset);
+		for (CVLine line : Mat) {
 
-}
+			for (char C : line)
+			{
+
+				if (C == FirstPlayer.m_token) Couleur(FirstPlayer.m_color);
+				if (C == SecondPlayer.m_token) Couleur(SecondPlayer.m_color);
+				if ((C == FirstPlayer.m_token) || (C == SecondPlayer.m_token)) Couleur(KReset);
+				cout << C;
+			}
+			cout << endl;
+		}
+	}
+
+	// WIN CHECK/STAT
+
+	bool CheckIfWin(SPlayer & FirstPlayer, SPlayer & SecondPlayer) {
+		return !((FirstPlayer.m_X > SecondPlayer.m_X + SecondPlayer.m_sizeX - 1) ||
+			(FirstPlayer.m_X + FirstPlayer.m_sizeX - 1 < SecondPlayer.m_X) ||
+			(SecondPlayer.m_Y > FirstPlayer.m_Y + FirstPlayer.m_sizeY - 1) ||
+			(SecondPlayer.m_Y + SecondPlayer.m_sizeY - 1 < FirstPlayer.m_Y));
+	}
+
+	char GetWinner(SPlayer& FirstPlayer, SPlayer &SecondPlayer, const unsigned & NbrTour) {
+		return (NbrTour % 2 == 0 ? FirstPlayer.m_token : SecondPlayer.m_token);
+	}
+
+
+	// BONUS
+
+	SBonus InitBonus(const unsigned largeur, const unsigned hauteur, const unsigned
+		AxeX, const unsigned AxeY, const char Token, const string Color) {
+
+		SBonus tmpBonus;
+
+		tmpBonus.m_sizeX = largeur;
+		tmpBonus.m_sizeY = hauteur;
+		tmpBonus.m_X = AxeX;
+		tmpBonus.m_Y = AxeY;
+		tmpBonus.m_token = Token;
+		tmpBonus.m_color = Color;
+
+		return tmpBonus;
+	}
+
+	void PutBonus(CMatrix & Matrice, SBonus & Bonus) {
+		Matrice[Bonus.m_Y][Bonus.m_X] = Bonus.m_token;
+	}
+
+	void GetBonus(CMatrix & Mat, SPlayer & Player) {
+		for (unsigned i(Player.m_Y); i < Player.m_Y + Player.m_sizeY; ++i) {
+			for (unsigned j(Player.m_X); j < Player.m_X + Player.m_sizeX; ++j) {
+				//Début de la détéction des bonus
+				
+				if (Mat[i][j] == 'B') {
+
+					++Player.m_sizeX;
+					++Player.m_sizeY;
+					
+				}
+
+			 	//Fin de la détéction des bonus
+				for (unsigned i(Player.m_Y); i < Player.m_Y + Player.m_sizeY; ++i)
+					for (unsigned j(Player.m_X); j < Player.m_X + Player.m_sizeX; ++j)
+						Mat[i][j] = Player.m_token;
+			}
+		}
+	}
+
+
+	// PLAYERS
+
+	SPlayer InitPlayer(const unsigned  largeur, const unsigned  hauteur, const unsigned  AxeX, const unsigned  AxeY, const char  Token, const string  Color) {
+
+		SPlayer tmpPlayer;
+
+		tmpPlayer.m_sizeX = largeur;
+		tmpPlayer.m_sizeY = hauteur;
+		tmpPlayer.m_X = AxeX;
+		tmpPlayer.m_Y = AxeY;
+		tmpPlayer.m_token = Token;
+		tmpPlayer.m_color = Color;
+
+		return tmpPlayer;
+	}
+
+	void MovePlayer(CMatrix & Mat, char Move, SPlayer & player) {
+		switch (Move)
+		{
+		case 'z':
+			if (player.m_Y > 0)
+			{
+				player.m_Y = player.m_Y - 1;
+				GetBonus(Mat, player);
+				for (unsigned i(player.m_X); i < player.m_X + player.m_sizeX; ++i)
+					Mat[player.m_Y + player.m_sizeY][i] = KEmpty;
+				for (unsigned i(player.m_X); i < player.m_X + player.m_sizeX; ++i)
+					Mat[player.m_Y][i] = player.m_token;
+			}
+			break;
+		case 's':
+			if (player.m_Y + player.m_sizeY < Mat.size())
+			{
+				player.m_Y = player.m_Y + 1;
+				GetBonus(Mat, player);
+				for (unsigned i(player.m_X); i < player.m_X + player.m_sizeX; ++i)
+					Mat[player.m_Y - 1][i] = KEmpty;
+				for (unsigned i(player.m_X); i < player.m_X + player.m_sizeX; ++i)
+					Mat[player.m_Y + player.m_sizeY - 1][i] = player.m_token;
+
+			}
+			break;
+		case 'q':
+			if (player.m_X > 0)
+			{
+				player.m_X = player.m_X - 1;
+				GetBonus(Mat, player);
+				for (unsigned i(player.m_Y); i < player.m_Y + player.m_sizeY; ++i)
+					Mat[i][player.m_X + player.m_sizeX] = KEmpty;
+				for (unsigned i(player.m_Y); i < player.m_Y + player.m_sizeY; ++i)
+					Mat[i][player.m_X] = player.m_token;
+			}
+			break;
+		case 'd':
+			if (player.m_X + player.m_sizeX < Mat[0].size())
+			{
+				player.m_X = player.m_X + 1;
+				GetBonus(Mat, player);
+				for (unsigned i(player.m_Y); i < player.m_Y + player.m_sizeY; ++i)
+					Mat[i][player.m_X - 1] = KEmpty;
+				for (unsigned i(player.m_Y); i < player.m_Y + player.m_sizeY; ++i)
+					Mat[i][player.m_X + player.m_sizeX - 1] = player.m_token;
+			}
+			break;
+		}
+	}
+
+	// OBSTACLES
+
+	SObstacle InitObstacle( const unsigned
+		AxeX, const unsigned AxeY, const char Token, const string Color) {
+
+		SObstacle tmpObstacle;
+
+		tmpObstacle.m_X = AxeX;
+		tmpObstacle.m_Y = AxeY;
+		tmpObstacle.m_token = Token;
+		tmpObstacle.m_color = Color;
+
+		return tmpObstacle;
+	}
+
+
+	void PutObstacle(CMatrix & Matrice, const SObstacle & obstacle) {
+		Matrice[obstacle.m_X - 1][obstacle.m_Y] = obstacle.m_token;
+	}
+
+	void GenerateRandomObstacles(CMatrix & Matrice, SObstacle & Obstacle, const unsigned & SizeObs) {
+		unsigned tmpX, tmpY;
+		for (unsigned i(0); i < SizeObs; ++i)
+		{
+			tmpX = Obstacle.m_X;
+			tmpY = Obstacle.m_Y;
+			int intrandom = IntRandom(1, 4);
+
+
+			if (intrandom == 1) {
+				Obstacle.m_X = tmpX + 1;
+				PutObstacle(Matrice, Obstacle);
+			}
+			else if (intrandom == 2) {
+				Obstacle.m_Y = tmpY + 1;
+				PutObstacle(Matrice, Obstacle);
+			}
+			else if (intrandom == 3) {
+				Obstacle.m_Y = tmpY - 1;
+				PutObstacle(Matrice, Obstacle);
+			}
+			else if (intrandom == 4) {
+				Obstacle.m_X = tmpX - 1;
+				PutObstacle(Matrice, Obstacle);
+			}
+
+		}
+	}
+
+
+
+
+	//AUTRE
+	unsigned AskTourMax() {
+		unsigned NbRnds;
+		cout << "Entrez le nombre de rounds" << endl;
+		cin >> NbRnds;
+		return NbRnds;
+	}
+	// todo
+	bool CanMove(SPlayer Joueur) {
+		return true;
+	}
+
 }
 
 int main()
 {
-    ppal ();
-    return 0;
+	const unsigned KSizeY(20);
+	const unsigned KSizeX(20);
+
+	unsigned NbRnds = AskTourMax();
+	char EnteredKey;
+
+
+	SPlayer FirstPlayer = InitPlayer(1, 1, 1, 1, 'X', KRouge);
+	SPlayer SecondPlayer = InitPlayer(1, 1, KSizeX - 2, KSizeY - 2, 'Y', KBleu);
+	SObstacle FirstObstacle = InitObstacle((KSizeX / 2), (KSizeX / 2 + 3), KObstacle, KMagenta);
+	SBonus FirstBonus = InitBonus(2, 2, 4, 4, 'B', KRouge);
+	CMatrix Map = InitMat(KSizeX + 1, KSizeY + 1, FirstPlayer, SecondPlayer, true); // +1 due à la bordure de '#' le long de la matrice
+
+
+	GenerateRandomObstacles(Map, FirstObstacle, 9);
+	PutBonus(Map, FirstBonus);
+
+
+	ShowMatrix(Map, FirstPlayer, SecondPlayer);
+	for (unsigned i(0); i < NbRnds * 2; ++i)
+	{
+
+		ShowMatrix(Map, FirstPlayer, SecondPlayer);
+		SPlayer & actualPlayer = (i % 2 == 0 ? FirstPlayer : SecondPlayer);
+
+		cout << "Au tour de " << actualPlayer.m_token << endl;
+		cin >> EnteredKey;
+
+
+		if (CanMove(actualPlayer))   MovePlayer(Map, EnteredKey, actualPlayer);
+
+		if (CheckIfWin(FirstPlayer, SecondPlayer))
+		{
+			cout << GetWinner(FirstPlayer, SecondPlayer, i) << " a gagné !" << endl;
+			return 0;
+		}
+	}
+	cout << "Egalité !" << endl;
+	return 0;
 }
